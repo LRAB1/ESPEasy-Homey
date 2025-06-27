5a25e655ae53eed677f3d98d1ac491fc09db47b4
Lex <lexbant@gmail.com>
2025-06-27 12:14:27 +0200

    logging succesful.

--- com.LRAB.ESPEasy/drivers/ducobox-mdns/device.ts
+++ com.LRAB.ESPEasy/drivers/ducobox-mdns/device.ts
@@ -44,6 +44,32 @@ this.registerCapabilityListener('fan_speed', async (speed) => {
     // Registering the capability listener for 'Box_mode_enum_cap'. Frontend.
     this.registerCapabilityListener('Box_mode_enum_cap', async (mode) => {
       this.log('Box_mode_enum_cap', mode);
+      setInterval(() => {
+    get('http://192.168.2.26/json?view=sensorupdate&tasknr=2', (res) => {
+      let data = '';
+      res.on('data', (chunk) => {
+        data += chunk;
+      });
+      res.on('end', () => {
+        try {
+          const jsonBody = JSON.parse(data);
+          const boxMode = jsonBody?.TaskValues?.[0]?.Value;
+          this.log('Parsed box mode:', boxMode);
+          this.setCapabilityValue('Box_mode_enum_cap', `${boxMode}`).catch(this.error);
+          if (boxMode === 0) {
+            this.setCapabilityValue('onoff', false).catch(this.error);
+          } else if (boxMode >= 3) {
+            this.setCapabilityValue('onoff', true).catch(this.error);
+          }
+        } catch (err) {
+          this.error('Failed to parse fan speed JSON:', err);
+        }
+      });
+    }).on('error', (err) => {
+      this.error('HTTP request error:', err);
+    });
+    this.log('Polling box mode...');
+  }, 10000); // 10 seconds
       if (mode >= 3) {
         this.log(`User requested setting: ${mode}`);
         get(`http://192.168.2.26/control?cmd=event,mode${mode}`);
@@ -52,9 +78,10 @@ this.registerCapabilityListener('fan_speed', async (speed) => {
       };
     });
 
-    this.setCapabilityValue('Box_mode_enum_cap', `0`).catch(this.error); // Setting the default value when first initializing the device.
+    this.triggerCapabilityListener('fan_speed', 0).catch(this.error);
+    this.triggerCapabilityListener('Box_mode_enum_cap', `0`).catch(this.error);
 
-    this.setCapabilityValue('fan_speed', 0); // Testcase.
+    // this.setCapabilityValue('Box_mode_enum_cap', `0`).catch(this.error); // Setting the default value when first initializing the device.
 
     this.setUnavailable(this.homey.__('device_unavailable')).catch(this.error);
 
